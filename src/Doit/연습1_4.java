3237d263d26dc2a298f33c69ce0e37534c198245
CHAEJYUN <nb00247@naver.com>
2023-06-13 11:58:48 +0900

    feat: Doit 문제 풀기

---
+++ src/Doit/연습1_4.java
@@ -0,0 +1,21 @@
+package Doit;
+
+public class 연습1_4 {
+	
+	static int med3(int a, int b, int c) {
+		if (a >= b) {
+			if (b >= c) return b;
+			else if (a <= c) return a;
+			else return c;
+		}else if (a > c) return a; //a >= b가 거짓이라면
+		else if (b > c)
+			return c;
+		else
+			return b;
+	}
+		
+		public static void main(String[] args) {
+			System.out.println("med3(3,2,1) = " + med3(3,2,1));
+	}
+
+}
