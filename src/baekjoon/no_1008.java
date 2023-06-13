e37120395684a8930820d392e55d99689d7e3419
CHAEJYUN <nb00247@naver.com>
2023-06-13 09:41:50 +0900

    feat: 문제 풀기

---
+++ src/baekjoon/no_1008.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class no_1008 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer token = new StringTokenizer(br.readLine());
+		double A = Integer.parseInt(token.nextToken());
+		double B = Integer.parseInt(token.nextToken());
+		System.out.println(A / B);
+	}
+
+}
