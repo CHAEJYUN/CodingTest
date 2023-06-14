e3813d848b91efc50a6eb100f4f0954986795d1a
CHAEJYUN <nb00247@naver.com>
2023-06-14 10:40:22 +0900

    feat: 백준 문제 풀기

---
+++ src/baekjoon/no_14681.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class no_14681 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int hour = h;
+		int minute = m - 45;
+		
+		if (minute < 0) {
+			hour--;
+			if (hour < 0) {
+				hour += 24;
+			}
+			minute += 60;
+		}
+		System.out.println(hour + " " + minute);
+	}
+
+}
---
+++ src/baekjoon/no_2525.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class no_2525 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		int cook = sc.nextInt();
+		
+		int min = h * 60 + m;
+		min += cook;
+		
+		int hour = (min / 60) % 24;
+		int minute = min % 60;
+		System.out.println(hour + " " + minute);
+	}
+
+}
